dd9792f7f5ab8c9c21fad2bc70f9d7f95d16532b
k4somy <kimrla125@gmail.com>
2015-02-19 20:22:14 -0600

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var homePage =
         '<div class="header"><h1>Page Slider</h1></div>' +
         '<div class="scroller">' +
             '<ul class="list">' +
-                '<li><a href="#page1"><strong>Build Bot</strong></a></li>' +
-                '<li><a href="#page2"><strong>Medi Bot</strong></a></li>' +
+                //'<li><a href="#page1"><strong>Build Bot</strong></a></li>' +
+                //'<li><a href="#page2"><strong>Medi Bot</strong></a></li>' +
                 '<li><a href="#page3"><strong>Ripple Bot</strong></a></li>' +
             '</ul>' +
         '</div>' +
